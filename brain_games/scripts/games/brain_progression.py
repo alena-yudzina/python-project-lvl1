d0106ec34952134735076f51221a5068ce0e8721
alena-yudzina <lenok.ok.lol@gmail.com>
2021-02-12 01:20:33 +0530

    deleted magical numbers

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -6,10 +6,12 @@ import prompt
 def randomize():
     random_start = random.randint(0, 20)
     random_step = random.randint(1, 7)
-    random_pos_hide = random.randint(0, 9)
+    random_prog_length = random.randint(5, 15)
+    random_pos_hide = random.randint(0, random_prog_length - 1)
     params = {
         'start_num': random_start,
         'step': random_step,
+        'length': random_prog_length,
         'hide_pos': random_pos_hide
     }
     return params
@@ -20,7 +22,8 @@ def game_task():
 
 
 def ask(params):
-    last_num = params['start_num'] + 10 * params['step']
+
+    last_num = params['start_num'] + params['length'] * params['step']
     progression = [str(x) for x in range(params['start_num'],
                    last_num, params['step'])]
     progression[params['hide_pos']] = '..'
